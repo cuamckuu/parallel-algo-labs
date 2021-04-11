849f0ee004a295a8f6ee98aaa5db91a0d09776df
Alex Kosh <sasha23969@gmail.com>
2021-04-11 22:43:45 +0300

    feat: Added plots creator

---
+++ locks-based-sets/plots/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env wpython
+
+import collections
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    data_txt = None
+    with open("data.txt", "r", encoding="utf-8") as file:
+        data_txt = file.read()
+
+    data = data_txt.split("===\n")[0:-1]
+
+    time_elapsed = collections.defaultdict(list)
+    for res in data:
+        threads, *algos = res.split("\n")
+
+        thread_num = int(threads.split(" ", 1)[0])
+
+        for algo in algos:
+            if not algo:
+                continue
+            name, time = algo.split(" ", 1)
+            time_elapsed[name].append((thread_num, float(time)))
+
+    for algo, data in time_elapsed.items():
+        xs, ys = zip(*data)
+        plt.plot(xs, ys, label=algo)
+
+    plt.title("Тесты CGS, FGS, OptSync")
+    plt.xlabel("Число потоков, шт")
+    plt.ylabel("Время выполнения, мс")
+    plt.legend()
+    plt.savefig("res.png")
